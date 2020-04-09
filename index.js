dac8afa586a8484cc1ea89157f10615e5b0c0b1d
YuichiYagisawa <yuichi9915@gmail.com>
2020-04-09 13:44:33 +0900

    create package

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+function add(numbers) {
+  let result = 0;
+  for (let num of numbers) {
+    result = result + num;
+  }
+  return result;
+}
+function multi(numbers){
+  let result = 0;
+  for (let num of numbers){
+    result = resule * num;
+  }
+  return result;
+}
+module.exports = {
+  add : add
+};
